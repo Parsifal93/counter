2dccebbd095f0270da0fdbb930ba76bbbcf378cc
Vadym Stratiienko <vadimstratiienko@gmail.com>
2021-10-10 22:23:02 +0300

    com

--- src/components/counter/index.jsx
+++ src/components/counter/index.jsx
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from 'react';
+import AutoClick from './AutoClick';
 
 export default function Count(props) {
     const [counter, setCounter] = useState(0);
@@ -12,9 +13,9 @@ const increment = () => {
 }
 
     const decrement = () =>{
-        setCounter((prevCounter)=> {
-            return prevCounter -1;
-        });
+        setCounter(counter - step) 
+         //   return prevCounter -1;
+        
     };  
 
     // const clickHandler = () =>{
@@ -29,37 +30,34 @@ const increment = () => {
         document.title = `Вы нажали ${counter} раз`;
       });
 
-    useEffect (() => {
-        const intervalID = setInterval(increment, 1000);
-        return()=> {
-            clearInterval(intervalID)
-        };
-        });
+    // useEffect (() => {
+    //    const intervalID = setInterval(increment, 1000);
+    //     return()=> {
+    //         clearInterval(intervalID)
+    //    };
+    //     },[increment]);
 
-        // useEffect(() => {
-        //     let timeoutID;
-        //      if (counter <= 0) {
-        //          timeoutID=setTimeout(increment,30000)
-        //      }
-        //      return () => {
-        //          clearTimeout(timeoutID)
-        //      }
-        //     });
-        
-
-   
-        
-    
+          useEffect(() => {
+              let timeoutID;
+               if (counter <= 29) {
+                   timeoutID=setTimeout(increment,1000)
+               }
+               return () => {
+                   clearTimeout(timeoutID)
+               }
+              });
+             
     return (
         <div>
            <div>Counter:{counter}</div> 
-            <input type="range" 
+          <div>  <input type="range" 
             name="step"
             value={step}
             min="1"
             max="10"
             onChange={handleStepChange}
-             />
+             /> 
+             </div>
              <div>
                <button onClick={decrement}>Prev</button>
                <button onClick={increment}>Next</button>
