b036fd2f38eecaf72064a2dfb8827ae659d561c7
Vadym Stratiienko <vadimstratiienko@gmail.com>
2021-10-10 22:22:08 +0300

    commit count

---
+++ src/components/counter/AutoClick/index.jsx
@@ -0,0 +1,25 @@
+import React, {useEffect, useState} from 'react';
+
+
+export default function AutoClick  (props) {
+    const[isStart] = useState(false);
+
+    useEffect(()=>{
+        function handleStatusChage(status){
+
+        }
+    })
+
+
+ 
+  
+        return (
+            <div>
+                <button>Auto/Stop</button>
+            </div>
+        )
+    
+        }
+
+
+
